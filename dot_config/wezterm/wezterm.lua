bfbdee0dfb7385f08f8908bced867533785a0b55
jimiaki7 <jimiaki7@gmail.com>
2024-10-26 20:39:42 +0900

    UPDATE

--- dot_config/wezterm/wezterm.lua
+++ dot_config/wezterm/wezterm.lua
@@ -63,5 +63,19 @@ config.keys = {
 	},
 }
 
+-- skipping confirmation on quit
+config.skip_close_confirmation_for_processes_named = {
+	"bash",
+	"sh",
+	"zsh",
+	"fish",
+	"tmux",
+	"nu",
+	"cmd.exe",
+	"pwsh.exe",
+	"powershell.exe",
+	"wezterm",
+}
+
 -- and finally, return the configuration to wezterm
 return config
